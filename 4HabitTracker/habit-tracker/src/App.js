53fdab367388ec50f512c91a66268b61f85442d8
Oscar <omankiewicz77@gmail.com>
2023-09-02 14:04:15 -0400

    basic structure

--- 4HabitTracker/habit-tracker/src/App.js
+++ 4HabitTracker/habit-tracker/src/App.js
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
 
+import HabitForm from "./components/HabitForm";
+import HabitList from "./components/HabitList";
+
+
+export default function App() {
+  const [habits, setHabits] = useState([]);
+
+  const addHabit = habitName => {
+    const newHabit = { name: habitName };
+    setHabits([...habits, newHabit]);
+  };
+
+  return (
+    <div className="habit-tracker">
+      <h1>Habit Tracker</h1>
+      <HabitForm onAdd={addHabit} />
+      <HabitList habits={habits} />
+    </div>
+  );
+}
